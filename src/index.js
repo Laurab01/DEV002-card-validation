9762e2126ebddd69178490cc5ef3e132e0072a2d
Laura Buitrago <buitrago92943@gmail.com>
2022-11-03 14:33:36 -0500

    Primer actualizacion

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 import validator from './validator.js';
 
-console.log(validator);
+
+document.getElementById("open").addEventListener("click", () => {
+    document.getElementById("contflotante").style.display = "block";
+});
+
+document.getElementById("close").addEventListener("click", () => {
+    document.getElementById("contflotante").style.display = "none";
+    document.getElementById("holder").value = "";
+    document.getElementById("number").value = "";
+    document.getElementById("cvc").value = "";
+
+})
+function maskify(CCardNumber) {
+    document.getElementById("finalCard").innerHTML = validator.maskify(CCardNumber);
+    document.getElementById("finalText").innerHTML = "La tarjeta es Valida.";
+}
+
+function tarjetaValida(creditCardNumber) {
+    let valid = validator.isValid(creditCardNumber);
+    if (valid === true) {
+        return true;
+
+    } else {
+        alert("Tarjeta Invalida")
+    }
+}
+
+function validar() {
+    let infoName = document.getElementById("holder").value;
+    let CCardNumber = document.getElementById("number").value;
+    let cvcNumber = document.getElementById("cvc").value;
+    if (infoName.length == "" || CCardNumber.length == "" || cvcNumber.length == "") {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+document.getElementById("subs").addEventListener("click", () => {
+    if (!validar()) {
+        alert("Verificar los espacios vacios");
+    }
+    else {
+        let CCardNumber = document.getElementById("number").value;
+        const validtarjeta = tarjetaValida(CCardNumber);
+        if (validtarjeta) {
+            maskify(CCardNumber);
+        }
+    }
+})
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,55 @@
 const validator = {
   // ...
-};
+  isValid: (CCardNumber) => {
+    let reversed = (CCardNumber.split("").reverse())
+    let posicionPar = 0;
+    let posicionImpar = 0;
+    let sum = 0;
+    for (let i = 0; i < reversed.length; i++) {
+      if ((i % 2) == 1) {
+        posicionPar = parseInt(reversed[i]) * 2;
+        if (posicionPar > 9) {
+          //10 = 1 //  (10 -9) + 1 //(10-10)+1
+          //16 = 7 // (16-9) + 1 // (16 - 10)+1
+          posicionPar = (posicionPar - 10) + 1;
+
+        }
+        sum += posicionPar;
+
+      }
+      else {
+        posicionImpar = parseInt(reversed[i]);
+        sum += posicionImpar;
+
+      }
+    }
+    //80 % 10 = 0
+    //81 % 10 = 1
+    if (sum % 10 == 0) {
+      return true;
+
+    }
+    else {
+      return false;
+    }
+  },
+
+  maskify: (CCardNumber) => {
+    let selectNumber = CCardNumber.length;
+    let maskingNumber = ""
+    for (let i = 0; i < selectNumber - 4; i++) {
+      maskingNumber += "#";
+    }
+    return maskingNumber + CCardNumber.substring(selectNumber - 4, selectNumber)
+
+  }
+
+}
+
 
 export default validator;
+
+// 4083952015263
+// 3625102593804
+
+// 6 + 6 + 4 + 5 + 2 + 0 + 4 + 5 + 9 + 3 + 7 + 0 + 8
